84241cd96b8794f316594238de7eff93b686862d
clayton7000 <80071108+clayton7000@users.noreply.github.com>
2024-10-11 23:58:08 -0300

    calculadora de soma
    pequena calculadora com opção de ler duas variaveis e calcular a soma delas

---
+++ calculadora de soma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a,b,c;
+	printf("informe o primeiro numero");
+	scanf("%d",&a);
+	printf("informe o segundo numero");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d+%d=%d",a,b,c);
+	return 0;
+}
